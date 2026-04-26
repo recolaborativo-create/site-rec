e01d2447abf5898a58ba86f835a457c98320b4c1
Henrique Callefi <henrique.callefi@gmail.com>
2026-04-26 17:38:11 -0300

    feat(config): astro config with sitemap integration

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,5 +1,12 @@
 // @ts-check
-import { defineConfig } from 'astro/config';
+import { defineConfig } from 'astro/config'
+import sitemap from '@astrojs/sitemap'
 
 // https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+  site: 'https://recolaborativo.com.br',
+  integrations: [sitemap()],
+  vite: {
+    css: { devSourcemap: true },
+  },
+})
